162d1792c26e68fb092bddd9fcf4715face110da
Daiki Arai <darai0512@yahoo.co.jp>
2024-04-08 21:57:00 +0900

    rate limit by PR #21

--- v1/examples/account/main.go
+++ v1/examples/account/main.go
@@ -1,36 +1,64 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
-	"strings"
-	"time"
-
+	"log"
 	"github.com/payjp/payjp-go/v1"
 )
 
-func main() {
-	payjpService := payjp.New("sk_test_c62fade9d045b54cd76d7036", nil)
+func display(resp interface{}) {
+	b, _ := json.Marshal(&resp)
+	var buf bytes.Buffer
+	json.Indent(&buf, b, "", "  ")
+	fmt.Println("RESPONSE:")
+	fmt.Println(buf.String())
+}
 
-	// Get Account
-	fmt.Println("Get Account")
-	account, _ := payjpService.Account.Retrieve()
-	fmt.Println("  Id:", account.ID)
-	fmt.Println("  CreatedAt: ", account.CreatedAt.Format(time.RFC1123Z))
-	fmt.Println("  Email: ", account.Email)
-	fmt.Println("  Merchant:")
-	fmt.Println("    Id", account.Merchant.ID)
-	fmt.Println("    BankEnabled:", account.Merchant.BankEnabled)
-	fmt.Println("    BrandsAccepted:", strings.Join(account.Merchant.BrandsAccepted, ", "))
-	fmt.Println("    ContactPhone:", account.Merchant.ContactPhone)
-	fmt.Println("    Country:", account.Merchant.Country)
-	fmt.Println("    CurrenciesSupported:", account.Merchant.CurrenciesSupported)
-	fmt.Println("    DefaultCurrency:", account.Merchant.DefaultCurrency)
-	fmt.Println("    DetailsSubmitted:", account.Merchant.DetailsSubmitted)
-	fmt.Println("    LiveModeActivatedAt:", account.Merchant.LiveModeActivatedAt.Format(time.RFC1123Z))
-	fmt.Println("    LiveModeEnabled:", account.Merchant.LiveModeEnabled)
-	fmt.Println("    ProductDetail:", account.Merchant.ProductDetail)
-	fmt.Println("    ProductName:", account.Merchant.ProductName)
-	fmt.Println("    SitePublished:", account.Merchant.SitePublished)
-	fmt.Println("    URL:", account.Merchant.URL)
-	fmt.Println("  TeamId: ", account.TeamID)
+func abortIfNeeded(err error, msg string) {
+	if err != nil {
+		panic(msg + err.Error())
+	}
+}
+
+func accountApi(pay *payjp.Service, out bool) {
+	// Account API の各エンドポイントを実行
+	fmt.Println("=== ACCOUNT API ===")
+	step := "*** RETRIEVE ACCOUNT ***"
+	fmt.Println(step)
+	resp, err := pay.Account.Retrieve()
+	abortIfNeeded(err, step)
+	if out {
+		display(resp)
+	}
+}
+
+func concurrentAccountApi(pay *payjp.Service, i int, c chan int) {
+	accountApi(pay, false)
+	c <- i
+}
+
+func con(pay *payjp.Service, x int) {
+	// 並列実行により意図的にテスト環境でレートリミットを発動させる
+	c := make(chan int)
+	for i := 0; i < x; i++ {
+		fmt.Println(i)
+		go concurrentAccountApi(pay, i, c)
+	}
+	fmt.Println(<-c)
+}
+
+func main() {
+	var buf bytes.Buffer
+	logger := log.New(&buf, "example_", log.Lshortfile)
+	retryConfig := payjp.Config{
+		MaxCount: payjp.Int(5),
+		// MaxDelay: payjp.Float(1),
+		// InitialDelay: payjp.Float(0.001),
+		Logger:   logger,
+	}
+	pay := payjp.New("sk_test_c62fade9d045b54cd76d7036", nil, retryConfig)
+	con(pay, 30)
+	fmt.Println(buf.String())
 }
